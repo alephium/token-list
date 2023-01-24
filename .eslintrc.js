07c69b54013a1af22bbaacaf109df2eba6e564e8
Thomas Droxler <2979182+tdroxler@users.noreply.github.com>
2023-01-24 08:16:27 +0100

    Apply suggestions from code review
    Co-authored-by: Ilias Trichopoulos <hlias.nop@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,29 +20,21 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
     'plugin:prettier/recommended',
-    'plugin:react/jsx-runtime'
   ],
-  plugins: ['react', '@typescript-eslint', 'prettier', 'react-hooks', 'unused-imports', 'simple-import-sort', 'header'],
+  plugins: ['@typescript-eslint', 'prettier', 'unused-imports', 'simple-import-sort', 'header'],
   rules: {
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-use-before-define': 'off',
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
     'no-use-before-define': 'off',
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies,
     'unused-imports/no-unused-imports-ts': 'warn',
     'simple-import-sort/imports': 'warn',
     'header/header': [2, './license-header.js'],
-    'jsx-quotes': ['warn', 'prefer-double'],
-    'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
     '@typescript-eslint/member-delimiter-style': [
       'error',
       {
@@ -57,21 +49,10 @@ module.exports = {
       }
     ]
   },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
   env: {
     browser: true,
     node: true,
     jasmine: true,
     jest: true
-  },
-  settings: {
-    react: {
-      pragma: 'React',
-      version: 'detect'
-    }
   }
 }
